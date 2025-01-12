08dd53d6c6a95d2444b0842148da661662cd3c3d
Tommy Larsson <45052383+larssont@users.noreply.github.com>
2025-01-12 17:53:55 +0100

    Remove catmull-rom implementation

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,4 @@
 const m = require('makerjs')
-const bezier = require('./bezier')
 
 exports.deepcopy = value => {
     if (value === undefined) return undefined
@@ -72,72 +71,6 @@ exports.poly = (points, curves = []) => {
     
     return model
 }
-
-const toCatmullRom = (points, alpha = 0.5) => {
-    if (!Array.isArray(points) || points.length < 4 || !points.every(point => Array.isArray(point) && point.length === 2)) {
-      throw new Error("Input must be an array of at least four points, each containing an x and y coordinate.");
-    }
-  
-    if (![0, 0.5, 1].includes(alpha)) {
-      throw new RangeError(`'alpha' should be: 0, 0.5, or 1. Got: ${alpha}`);
-    }
-  
-    let p0, p1, p2, p3, bp1, bp2, d1, d2, d3, A, B, N, M;
-    let d3powA, d2powA, d3pow2A, d2pow2A, d1pow2A, d1powA;
-    let result = [];
-  
-    for (let i = 0; i < points.length - 1; i++) {
-      p0 = i === 0 ? points[0] : points[i - 1];
-      p1 = points[i];
-      p2 = points[i + 1];
-      p3 = i + 2 < points.length ? points[i + 2] : p2;
-  
-      d1 = Math.sqrt(Math.pow(p0[0] - p1[0], 2) + Math.pow(p0[1] - p1[1], 2));
-      d2 = Math.sqrt(Math.pow(p1[0] - p2[0], 2) + Math.pow(p1[1] - p2[1], 2));
-      d3 = Math.sqrt(Math.pow(p2[0] - p3[0], 2) + Math.pow(p2[1] - p3[1], 2));
-  
-      // Catmull-Rom to Cubic Bezier conversion matrix
-      d3powA = Math.pow(d3, alpha);
-      d3pow2A = Math.pow(d3, 2 * alpha);
-      d2powA = Math.pow(d2, alpha);
-      d2pow2A = Math.pow(d2, 2 * alpha);
-      d1powA = Math.pow(d1, alpha);
-      d1pow2A = Math.pow(d1, 2 * alpha);
-  
-      A = 2 * d1pow2A + 3 * d1powA * d2powA + d2pow2A;
-      B = 2 * d3pow2A + 3 * d3powA * d2powA + d2pow2A;
-      N = 3 * d1powA * (d1powA + d2powA);
-  
-      if (N > 0) N = 1 / N;
-      M = 3 * d3powA * (d3powA + d2powA);
-  
-      if (M > 0) M = 1 / M;
-  
-      // Calculate control points
-      bp1 = [
-        (-d2pow2A * p0[0] + A * p1[0] + d1pow2A * p2[0]) * N,
-        (-d2pow2A * p0[1] + A * p1[1] + d1pow2A * p2[1]) * N,
-      ];
-  
-      bp2 = [
-        (d3pow2A * p1[0] + B * p2[0] - d2pow2A * p3[0]) * M,
-        (d3pow2A * p1[1] + B * p2[1] - d2pow2A * p3[1]) * M,
-      ];
-  
-      // If the control points are the same as the points, leave them as is
-      if (bp1[0] === 0 && bp1[1] === 0) bp1 = p1;
-      if (bp2[0] === 0 && bp2[1] === 0) bp2 = p2;
-  
-      // Add the curve to the result array
-      result.push([
-        [bp1[0], bp1[1]],
-        [bp2[0], bp2[1]],
-        [p2[0], p2[1]],
-      ]);
-    }
-  
-    return result;
-  };  
 
 exports.bbox = (arr) => {
     let minx = Infinity
